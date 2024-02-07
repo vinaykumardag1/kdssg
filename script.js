0c16064786ff46ddd19606586f0e1020368e2f68
dagguapti vinaykumar <112471376+vinaykumardag1@users.noreply.github.com>
2024-02-07 15:15:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function moreaboutus(){
+    document.getElementById('readmoreabtus').style.display="none";
+    document.getElementById('readlessabtus').style.display="block"; 
+}
+function lessaboutus(){
+    document.getElementById('readmoreabtus').style.display="block";
+    document.getElementById('readlessabtus').style.display="none"; 
+}
