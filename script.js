e4982b5ad283f4e76062936fc040a754875a7a61
dagguapti vinaykumar <112471376+vinaykumardag1@users.noreply.github.com>
2024-02-08 13:12:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function lessaboutus(){
     document.getElementById('readmoreabtus').style.display="block";
     document.getElementById('readlessabtus').style.display="none"; 
 }
+ document.addEventListener("DOMContentLoaded", function() {
+        var modal = document.getElementById('exampleModal');
+        var modalInstance = new bootstrap.Modal(modal);
+        modalInstance.show();
+    });
